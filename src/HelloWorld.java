01ae4d38eee0e48654e3c778087f73a3ce0510ff
Zuhal Acikgoz <zacikgoz@hotmail.com>
2022-01-10 11:30:30 -0600

    work 1 is done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,10 @@ public class HelloWorld {
 
    //Doing some work
 
+    public static void main(String[] args) {
+        String str = "Hello World";
+        System.out.println(str);
+    }
 
 
 }
